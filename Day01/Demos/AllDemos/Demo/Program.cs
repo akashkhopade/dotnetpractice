0edd0484c0bd52d34406e39f591a8868705ca094
akashkhopade <113437668+akashkhopade@users.noreply.github.com>
2023-01-09 16:03:52 +0530

    Add files via upload

---
+++ Day01/Demos/AllDemos/Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Data.SqlClient;
+using System;
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //select comment code: ctrl+k+c
+            //select comment code: ctrl+k+u
+
+            //SqlConnection connection;
+            //SqlCommand statement;
+            //SqlDataReader resultset;
+
+            //C# Code
+
+            int i = 100;
+            int j = 200;
+            int result = i + j;
+
+            Console.WriteLine(result);
+            Console.ReadLine(); //proceed when new line character comes up on screen ..i.e. enter
+
+            //Note: On Developer Command Prompt, Use ildasm <EXE path> to see the MSIL Code
+            //ildasm means Intermediate Language Dis ASseMbler
+        }
+    }
+}
